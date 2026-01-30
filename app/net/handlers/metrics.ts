26921f3bff6e946350085387a902ab1400d6cf81
MairwunNx <personal@mairwunnx.com>
2026-01-30 19:37:10 +0300

    🔧 Исправлен заголовок Content-Type в обработчике метрик

--- app/net/handlers/metrics.ts
+++ app/net/handlers/metrics.ts
@@ -4,6 +4,6 @@ import { exportMetrics } from "@/svc/metrics/service";
 export const metricsHandler = async (): Promise<Response> => {
   const metrics = await exportMetrics();
   return new Response(metrics, {
-    headers: { "Content-Type": registry.contentType }
+    headers: { "Content-Type": registry.contentType },
   });
 };

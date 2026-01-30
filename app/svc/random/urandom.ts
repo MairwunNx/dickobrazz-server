ff3911b59a02d91d484118cff18192a7e6901c4d
MairwunNx <personal@mairwunnx.com>
2026-01-30 19:43:18 +0300

    🆕 Добавлена функция secureRandomInRange для генерации безопасных случайных чисел в заданном диапазоне

---
+++ app/svc/random/urandom.ts
@@ -0,0 +1,23 @@
+export const secureRandomInRange = (min: number, max: number): number => {
+  if (min >= max) throw new Error("Max must be greater than min");
+
+  const range = max - min + 1;
+  const ceiling = 2 ** Math.ceil(Math.log2(range));
+
+  const array = new Uint8Array(ceiling <= 256 ? 1 : ceiling <= 65536 ? 2 : 4);
+  let randomNumber: number;
+
+  do {
+    crypto.getRandomValues(array);
+
+    if (array.length === 1) {
+      randomNumber = array[0] as number;
+    } else if (array.length === 2) {
+      randomNumber = (array[0] as number) | ((array[1] as number) << 8);
+    } else {
+      randomNumber = (array[0] as number) | ((array[1] as number) << 8) | ((array[2] as number) << 16) | ((array[3] as number) << 24);
+    }
+  } while (randomNumber >= ceiling);
+
+  return min + (randomNumber % range);
+};

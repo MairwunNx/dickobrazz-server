ba5c5caf52dd92e9ef40319768c1a340764a3c69
MairwunNx <personal@mairwunnx.com>
2026-01-30 19:27:54 +0300

    🆕 Добавлен обработчик метрик для экспорта данных Prometheus

---
+++ app/net/handlers/metrics.ts
@@ -0,0 +1,9 @@
+import { registry } from "@/stt/prometheus";
+import { exportMetrics } from "@/svc/metrics/service";
+
+export const metricsHandler = async (): Promise<Response> => {
+  const metrics = await exportMetrics();
+  return new Response(metrics, {
+    headers: { "Content-Type": registry.contentType }
+  });
+};

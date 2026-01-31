ee29d98bbbfd8477d43bf0efbeb2feaf9273c817
MairwunNx <personal@mairwunnx.com>
2026-01-31 13:24:51 +0300

    🛠️ Удалены поля next_cursor и prev_cursor из схемы пагинации для упрощения структуры данных

--- app/dto/pagination.ts
+++ app/dto/pagination.ts
@@ -3,8 +3,6 @@ import { z } from "zod";
 export const PageMetaSchema = z.object({
   limit: z.number().optional(),
   page: z.number().optional(),
-  next_cursor: z.string().optional(),
-  prev_cursor: z.string().optional(),
   total: z.number().optional(),
 });
 

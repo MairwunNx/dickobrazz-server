18dc213100c9039e49bd17cc7e071f1ed5e93ad9
MairwunNx <personal@mairwunnx.com>
2026-01-31 13:15:32 +0300

    ✨ Добавлены новые схемы для достижений, динамики, лидерборда, гонок и сезонов

---
+++ app/dto/cock/index.ts
@@ -0,0 +1,42 @@
+export { type Achievement, AchievementSchema, type CockAchievementsResponse, CockAchievementsResponseSchema } from "./achievements";
+export {
+  type CockDynamicDailyDynamics,
+  CockDynamicDailyDynamicsSchema,
+  type CockDynamicFiveCocksDynamics,
+  CockDynamicFiveCocksDynamicsSchema,
+  type CockDynamicIndividual,
+  CockDynamicIndividualSchema,
+  type CockDynamicOverall,
+  CockDynamicOverallSchema,
+  type CockDynamicPercentile,
+  CockDynamicPercentileSchema,
+  type CockDynamicRecentStat,
+  CockDynamicRecentStatSchema,
+  type CockDynamicRecord,
+  CockDynamicRecordSchema,
+  type CockDynamicResponse,
+  CockDynamicResponseSchema,
+} from "./dynamics";
+export {
+  type CockLadderResponse,
+  CockLadderResponseSchema,
+  type CockRulerResponse,
+  CockRulerResponseSchema,
+  type LeaderboardEntry,
+  LeaderboardEntrySchema,
+  type UserNeighborhood,
+  UserNeighborhoodSchema,
+} from "./leaderboard";
+export { type CockRaceResponse, CockRaceResponseSchema, type RaceEntry, RaceEntrySchema, type RaceNeighborhood, RaceNeighborhoodSchema } from "./race";
+
+export {
+  type CockSeasonsResponse,
+  CockSeasonsResponseSchema,
+  type SeasonInfo,
+  SeasonInfoSchema,
+  type SeasonWinner,
+  SeasonWinnerSchema,
+  type SeasonWithWinners,
+  SeasonWithWinnersSchema,
+} from "./seasons";
+export { type CockSizeResponse, CockSizeResponseSchema } from "./size";

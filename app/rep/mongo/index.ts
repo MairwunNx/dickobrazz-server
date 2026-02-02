e9a74ad1e74641bf9362678fdf1cbf95daba6482
MairwunNx <personal@mairwunnx.com>
2026-02-02 14:19:55 +0300

    🔧 Обновлён код импорта: добавлены новые запросы и изменён экспорт для улучшения структуры кода

--- app/rep/mongo/index.ts
+++ app/rep/mongo/index.ts
@@ -1,1 +1,9 @@
-export { createIndexes } from "./indexes";
+export { index } from "./indexes";
+export * from "./queries/achievements";
+export * from "./queries/cock";
+export * from "./queries/dynamic";
+export * from "./queries/ladder";
+export * from "./queries/metrics";
+export * from "./queries/race";
+export * from "./queries/seasons";
+export * from "./queries/users";

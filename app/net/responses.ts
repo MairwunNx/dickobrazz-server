72886997a984fb1b46acc48748328f062fd794bd
MairwunNx <personal@mairwunnx.com>
2026-02-02 13:51:24 +0300

    🔧 Переименованы функции обработки ответов: successResponse на success и errorResponse на failure

--- app/net/responses.ts
+++ app/net/responses.ts
@@ -1,11 +1,11 @@
 import type { APIError, APIResponse } from "@/dto/response";
 
-export const successResponse = <T>(data: T, status = 200): Response => {
+export const success = <T>(data: T, status = 200): Response => {
   const response: APIResponse<T> = { data };
   return Response.json(response, { status });
 };
 
-export const errorResponse = (message: string, code = "ERROR", status = 400): Response => {
+export const failure = (message: string, code = "ERROR", status = 400): Response => {
   const error: APIError = { message, code };
   const response: APIResponse = { error };
   return Response.json(response, { status });

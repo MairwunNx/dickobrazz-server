7002048c95ad38280844d4cfc4ebabe75a4b8cf1
Pavel Erokhin <mairwunnx@gmail.com>
2026-02-11 23:58:39 +0300

    ✨ Добавлен комментарий в файл get-respect.action.ts с TODO для добавления новых полей, связанных с количеством респектов за победы в сезоне и выполненные ачивки, а также для сохранения total_respect.

--- src/features/respects/get-respect.action.ts
+++ src/features/respects/get-respect.action.ts
@@ -1,5 +1,6 @@
 import type { RespectResponse } from "./types";
 
+// todo: добавить кол-во респектов за победы в сезоне и за выполненные ачивки (два отдельных еще поля) ну и + total_respect так и остается
 export const createGetRespectAction = () => async (): Promise<RespectResponse> => ({
   total_respect: 0,
 });

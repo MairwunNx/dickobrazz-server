73f609ed5fc54df647a23b8bd33ae8a665b2b56e
Pavel Erokhin <mairwunnx@gmail.com>
2026-02-12 18:19:58 +0300

    ✨ Добавлено поле `pulled_at` в схему ответа `CockSizeResponseSchema`, что улучшает отслеживание времени получения данных о размере. Это повышает управляемость информации о пользователе.

--- src/features/cock-size/types.ts
+++ src/features/cock-size/types.ts
@@ -4,6 +4,7 @@ export const CockSizeResponseSchema = z.object({
   size: z.number(),
   hash: z.string(),
   salt: z.string(),
+  pulled_at: z.string(),
 });
 
 export type CockSizeResponse = z.infer<typeof CockSizeResponseSchema>;
